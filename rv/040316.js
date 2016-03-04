fa599d312b572e8a911bd54ae9f6eefd2adc4dac
CarlosMDC <porscheelmejor@hotmail.com>
2016-03-04 14:19:41 -0600

    Update 040316.js

--- rv/040316.js
+++ rv/040316.js
@@ -27,7 +27,7 @@ function setup(){
   escena.add(cuerpo);
   escena.add(piernaD);
   escena.add(piernaI);
-  camara= new THREE.Perspective.Camera();
+  camara= new THREE.PerspectiveCamera();
   camara.postion.z=20;
   
   renderer=new THREE.WebGLRenderer();
