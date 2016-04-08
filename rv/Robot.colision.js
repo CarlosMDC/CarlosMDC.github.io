783df7835e854816015d1825c7769339e2f68305
CarlosMDC <porscheelmejor@hotmail.com>
2016-04-08 13:25:12 -0500

    Update Robot.colision.js

--- rv/Robot.colision.js
+++ rv/Robot.colision.js
@@ -1,9 +1,9 @@
 function setup() {
 
-	Pared1= new THREE.Mesh(new THREE.BoxGeometry(1,50,1),new THREE.MeshNormalMaterial());
-	Pared2= new THREE.Mesh(new THREE.BoxGeometry(1,50,1),new THREE.MeshNormalMaterial());
-	Pared3= new THREE.Mesh(new THREE.BoxGeometry(50,1,1),new THREE.MeshNormalMaterial());
-	Pared4= new THREE.Mesh(new THREE.BoxGeometry(50,1,1),new THREE.MeshNormalMaterial());
+	Pared1= new THREE.Mesh(new THREE.BoxGeometry(10,100,1),new THREE.MeshNormalMaterial());
+	Pared2= new THREE.Mesh(new THREE.BoxGeometry(10,100,1),new THREE.MeshNormalMaterial());
+	Pared3= new THREE.Mesh(new THREE.BoxGeometry(100,10,1),new THREE.MeshNormalMaterial());
+	Pared4= new THREE.Mesh(new THREE.BoxGeometry(100,10,1),new THREE.MeshNormalMaterial());
 	
 
 	var cuboForma= new THREE.BoxGeometry(10,10,10);
@@ -65,10 +65,7 @@ function setup() {
 	 THREE.GeometryUtils.merge(forma, triangulo2);
  
 	 var material =new THREE.MeshNormalMaterial();
-	 malla= new THREE.Mesh(forma, material);
-
-
-	//pelota= new THREE.Mesh(new THREE.SphereGeometry(0.5),new THREE.MeshNormalMaterial());
+	 wally= new THREE.Mesh(forma, material);
 
 	Pared1.position.x=50;
 	Pared2.position.x=-50;
@@ -78,8 +75,10 @@ function setup() {
 	camara = new THREE.PerspectiveCamera();
 	camara.position.z=200;
 
-	raycaster1= new THREE.Raycaster(malla.position, new THREE.Vector3(1,0,0));
-	raycaster2= new THREE.Raycaster(malla.position, new THREE.Vector3(-1,0,0));
+	raycaster1= new THREE.Raycaster(wally.position, new THREE.Vector3(1,0,0));
+	raycaster2= new THREE.Raycaster(wally.position, new THREE.Vector3(-1,0,0));
+	raycaster3= new THREE.Raycaster(wally.position, new THREE.Vector3(0,1,0));
+	raycaster4= new THREE.Raycaster(wally.position, new THREE.Vector3(0,-1,0));
 	
 	escena= new THREE.Scene();
 	escena.add(Pared1);
@@ -87,12 +86,13 @@ function setup() {
 	escena.add(Pared3);
 	escena.add(Pared4);
 	escena.add(camara);
-	escena.add(pelota);
+	escena.add(wally);
 
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
  	document.body.appendChild(renderer.domElement);
-	step=0.1;
+	step=0.9;
+	step2=0.01;
  }
 
 
@@ -100,25 +100,47 @@ function loop() {
 
 	obstaculo1= raycaster1.intersectObject(Pared1);
 	obstaculo2= raycaster2.intersectObject(Pared2);
-	obstaculo3= raycaster2.intersectObject(Pared3);
-	obstaculo4= raycaster2.intersectObject(Pared4);
+	obstaculo3= raycaster3.intersectObject(Pared3);
+	obstaculo4= raycaster4.intersectObject(Pared4);
 
-	if((obstaculo1.length>0 && (obstaculo1[0].distance<=0.5)) || 
-	   (obstaculo2.length>0 && (obstaculo2[0].distance<=0.5)))	
+	//wally.position.x +=step;
+
+	if((obstaculo1.length>0 && (obstaculo1[0].distance<=10)))
 	step=-step;
+	wally.position.x +=step;
 
-	malla.position.x +=step;
+		if((obstaculo1.length>0 && (obstaculo1[0].distance>1)))
+		step=step;
+		wally.position.y +=step;
+	
+			if((obstaculo3.length>0 && (obstaculo3[0].distance<=10)))
+			//step=step;
+			wally.position.x -=step;
 
-	raycaster1.set(malla.position, new THREE.Vector3(1,0,0))
-	raycaster2.set(malla.position, new THREE.Vector3(-1,0,0))
+				if((obstaculo2.length>0 && (obstaculo2[0].distance<=10)))
+				//step=step;
+				wally.position.y -=step;
+	
+					if((obstaculo4.length>0 && (obstaculo4[0].distance<=10)))
+					//step=step;
+					wally.position.x +=step;
+					
+				
+			
+		
+
+
+	raycaster1.set(wally.position, new THREE.Vector3(1,0,0))
+	raycaster2.set(wally.position, new THREE.Vector3(-1,0,0))
+	raycaster3.set(wally.position, new THREE.Vector3(0,1,0))
+	raycaster4.set(wally.position, new THREE.Vector3(0,-1,0))
 
 	requestAnimationFrame(loop);
 	renderer.render(escena,camara);
 }
 
-var escena, camara, renderer, Pared1, Pared2,Pared3,Pared4, malla;
-var raycaster1, reycaster2,step;
+var escena, camara, renderer, Pared1, Pared2,Pared3,Pared4, wally;
+var raycaster1, raycaster2, raycaster3,raycaster4,step,step2;
 var obstaculo1, obstaculo2,obstaculo3,obstaculo4;
 setup();
 loop();
-
